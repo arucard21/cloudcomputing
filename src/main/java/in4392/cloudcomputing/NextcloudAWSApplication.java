a2123599c00082547b416e498798d7e0ff4a7584
arucard21 <arucard21@gmail.com>
2018-10-12 05:59:07 +0200

    Initial project setup

---
+++ src/main/java/in4392/cloudcomputing/NextcloudAWSApplication.java
@@ -0,0 +1,11 @@
+package in4392.cloudcomputing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextcloudAWSApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NextcloudAWSApplication.class, args);
+	}
+}
