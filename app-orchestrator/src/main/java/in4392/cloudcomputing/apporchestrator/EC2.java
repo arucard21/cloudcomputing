4deffa6f06093ce234434b27b10dabd350314306
arucard21 <arucard21@gmail.com>
2018-10-23 09:16:04 +0200

    Extract EC2 deployment code to separate class and copy to app-orchestrator

---
+++ app-orchestrator/src/main/java/in4392/cloudcomputing/apporchestrator/EC2.java
@@ -0,0 +1,238 @@
+package in4392.cloudcomputing.apporchestrator;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.inject.Named;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.UriBuilder;
+
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.model.DeleteKeyPairRequest;
+import com.amazonaws.services.ec2.model.DescribeInstanceStatusRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.ImportKeyPairRequest;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.InstanceStatus;
+import com.amazonaws.services.ec2.model.InstanceType;
+import com.amazonaws.services.ec2.model.ResourceType;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
+import com.amazonaws.services.ec2.model.StartInstancesRequest;
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+import com.amazonaws.services.ec2.model.SummaryStatus;
+import com.amazonaws.services.ec2.model.Tag;
+import com.amazonaws.services.ec2.model.TagSpecification;
+import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
+
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.Session.Command;
+import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
+import net.schmizz.sshj.xfer.FileSystemFile;
+import net.schmizz.sshj.xfer.scp.SCPFileTransfer;
+
+@Named
+public class EC2 {
+	private static final String AWS_KEYPAIR_NAME = "accessibleFromMainInstance";
+	private static final String AMI_ID_EU_WEST_3_UBUNTU_SERVER_1804 = "ami-0a2ca21adb4a04084";
+	private static final String ERROR_INCORRECTLY_DEPLOYED = "The newly deployed EC2 instance did not start correctly. You may need to manually verify and correct this";
+	private static final String ERROR_STATUS_CHECKS_NOT_OK = "The newly deployed EC2 instance is running but some of the status checks may not have passed";
+	private static final int ITERATION_WAIT_TIME = 60 * 1000;
+	private static final int INSTANCE_PENDING = 0;
+	private static final int INSTANCE_RUNNING = 16;
+	private static final int INSTANCE_STOPPED = 80;
+	private static AmazonEC2 client;
+	private static KeyPair javaKeyPair;
+	
+	public static void startEC2Instance(String instanceId) {
+		client.startInstances(new StartInstancesRequest().withInstanceIds(instanceId));
+	}
+
+	public static boolean isStopped(String instanceId) {
+		Instance stoppingInstance = retrieveEC2InstanceWithId(instanceId);
+		if (stoppingInstance.getState().getCode() == INSTANCE_STOPPED) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void stopEC2Instance(String instanceId) {
+		client.stopInstances(new StopInstancesRequest().withInstanceIds(instanceId));
+	}
+
+	public static void terminateEC2(String instanceId) {
+		client.terminateInstances(new TerminateInstancesRequest().withInstanceIds(instanceId));
+	}
+
+	public static Instance retrieveEC2InstanceWithId(String instanceId) {
+		return client.describeInstances(new DescribeInstancesRequest()
+				.withInstanceIds(instanceId))
+				.getReservations().get(0)
+				.getInstances().get(0);
+	}
+
+	public static int healthCheckOnInstance(Instance instance) {
+		URI mainInstanceHealth = UriBuilder.fromUri(instance.getPublicDnsName()).path("health").build();
+		int httpStatus = ClientBuilder.newClient().target(mainInstanceHealth).request().get().getStatus();
+		return httpStatus;
+	}
+
+	/**
+	 * Deploy an EC2 instance and wait for it to be running and the status checks to pass
+	 * 
+	 * @param usageTag is the tag that represents what the machine will be used for
+	 * @return the Instance object representing the deployed instance
+	 * @throws NoSuchAlgorithmException 
+	 * @throws IOException if the userdata script can not be read
+	 */
+	public static Instance deployDefaultEC2(String usageTag, String keyPairName) throws NoSuchAlgorithmException {
+		ensureJavaKeyPairExists();
+		RunInstancesRequest runInstancesRequest = new RunInstancesRequest(AMI_ID_EU_WEST_3_UBUNTU_SERVER_1804, 1, 1)
+				.withInstanceType(InstanceType.T2Micro)
+				.withKeyName(keyPairName)
+				.withUserData(getUserData());
+		if (usageTag != null && !usageTag.isEmpty()) {
+			runInstancesRequest.withTagSpecifications(
+					new TagSpecification()
+					.withResourceType(ResourceType.Instance)
+					.withTags(
+							new Tag()
+							.withKey("Usage")
+							.withValue(usageTag)));
+		}
+		RunInstancesResult runInstancesResult = client.runInstances(runInstancesRequest);
+		String deployedInstanceId = runInstancesResult.getReservation().getInstances().get(0).getInstanceId();
+		// wait up to 1 minute for the instance to run
+		waitForInstanceToRun(deployedInstanceId);
+		return client.describeInstances(new DescribeInstancesRequest().withInstanceIds(deployedInstanceId))
+				.getReservations().get(0)
+				.getInstances().get(0);
+	}
+
+	public static void ensureJavaKeyPairExists() throws NoSuchAlgorithmException {
+		if (javaKeyPair != null) {
+			return;
+		}
+		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+		generator.initialize(2048);
+		javaKeyPair = generator.generateKeyPair();
+		String publicKey = Base64.getEncoder().encodeToString(javaKeyPair.getPublic().getEncoded());
+		removeExistingKeyPair();
+		ImportKeyPairRequest keyPairRequest = new ImportKeyPairRequest(AWS_KEYPAIR_NAME, publicKey);
+		client.importKeyPair(keyPairRequest);
+	}
+
+	public static void removeExistingKeyPair() {
+		client.deleteKeyPair(new DeleteKeyPairRequest(AWS_KEYPAIR_NAME));
+	}
+
+	public static String getUserData() {
+		String userData = "#!/bin/bash\n" + 
+				"apt update\n" + 
+				"apt install -y openjdk-8-jre\n";
+		return Base64.getEncoder().encodeToString(userData.getBytes());
+	}
+
+	public static void waitForInstanceToRun(String deployedInstanceId) {
+		for (int i = 0; i < 6; i++) {
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			int state = getInstanceState(deployedInstanceId);
+			if (state == INSTANCE_PENDING) {
+				continue;
+			}
+			if (state == INSTANCE_RUNNING) {
+				// check status checks are completed as well
+				boolean passed = verifyStatusChecksPassed(deployedInstanceId);
+				if (!passed) {
+				        throw new IllegalStateException(ERROR_STATUS_CHECKS_NOT_OK);
+				}
+				break;
+			}
+			throw new IllegalStateException(ERROR_INCORRECTLY_DEPLOYED);
+		}
+	}
+
+	public static int getInstanceState(String deployedInstanceId) {
+		int state = client.describeInstances(new DescribeInstancesRequest().withInstanceIds(deployedInstanceId))
+				.getReservations().get(0)
+				.getInstances().get(0)
+				.getState()
+				.getCode();
+		return state;
+	}
+	
+	public static boolean verifyStatusChecksPassed(String deployedInstanceId) {
+		boolean passed = false;
+		// wait up to 10 minutes for the status checks
+		for (int j = 0; j < 10; j++) {
+			try {
+				Thread.sleep(60000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			InstanceStatus status = client
+					.describeInstanceStatus(new DescribeInstanceStatusRequest().withInstanceIds(deployedInstanceId))
+					.getInstanceStatuses().get(0);
+			SummaryStatus systemStatus = SummaryStatus.fromValue(status.getSystemStatus().getStatus());
+			SummaryStatus instanceStatus = SummaryStatus.fromValue(status.getInstanceStatus().getStatus());
+			if (SummaryStatus.Ok.equals(systemStatus) && SummaryStatus.Ok.equals(instanceStatus)) {
+				passed = true;
+				break;
+			}
+		}
+		return passed;
+	}
+
+	public static void copyApplicationToDeployedInstance(File applicationJarFile, Instance instance) throws IOException, NoSuchAlgorithmException {
+		ensureJavaKeyPairExists();
+		try (SSHClient ssh = new SSHClient()){
+			ssh.loadKnownHosts();
+			ssh.addHostKeyVerifier(new PromiscuousVerifier());
+			ssh.connect(instance.getPublicDnsName());
+			ssh.authPublickey("ubuntu", Arrays.asList(ssh.loadKeys(javaKeyPair)));
+			SCPFileTransfer scp = ssh.newSCPFileTransfer();
+			String homeDirOnRemoteInstance = "/home/ubuntu/";
+			scp.upload(new FileSystemFile(applicationJarFile), homeDirOnRemoteInstance);
+		}
+	}
+
+	public static void startDeployedApplication(Instance instance, String applicationName) throws IOException, NoSuchAlgorithmException {
+		ensureJavaKeyPairExists();
+		try (SSHClient ssh = new SSHClient()){
+			ssh.loadKnownHosts();
+			ssh.addHostKeyVerifier(new PromiscuousVerifier());
+			ssh.connect(instance.getPublicDnsName());
+			ssh.authPublickey("ubuntu", Arrays.asList(ssh.loadKeys(javaKeyPair)));
+			Command command;
+			String remoteCommand = "nohup java -jar /home/ubuntu/"+applicationName+".jar > /home/ubuntu/"+applicationName+".log 2>&1 &";
+			try(Session session = ssh.startSession()){
+				command = session.exec(remoteCommand);
+			}
+			if (command.getExitStatus() != 0) {
+				System.out.println(applicationName + " was not started correctly with the command: " + remoteCommand);
+			}
+		}
+	}
+	
+	/**
+	 * Wait a specific amount of time before
+	 */
+	public static void waitUntilNextIteration() {
+		try {
+			Thread.sleep(ITERATION_WAIT_TIME);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- main-instance/src/main/java/in4392/cloudcomputing/maininstance/MainInstance.java
+++ main-instance/src/main/java/in4392/cloudcomputing/maininstance/MainInstance.java
@@ -1,16 +1,11 @@
 package in4392.cloudcomputing.maininstance;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Paths;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
@@ -19,8 +14,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.inject.Named;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.UriBuilder;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -32,43 +25,16 @@ import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsRequest;
 import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsResult;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
-import com.amazonaws.services.ec2.model.DeleteKeyPairRequest;
-import com.amazonaws.services.ec2.model.DescribeInstanceStatusRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
-import com.amazonaws.services.ec2.model.ImportKeyPairRequest;
 import com.amazonaws.services.ec2.model.Instance;
-import com.amazonaws.services.ec2.model.InstanceStatus;
-import com.amazonaws.services.ec2.model.InstanceType;
 import com.amazonaws.services.ec2.model.Reservation;
-import com.amazonaws.services.ec2.model.ResourceType;
-import com.amazonaws.services.ec2.model.RunInstancesRequest;
-import com.amazonaws.services.ec2.model.RunInstancesResult;
-import com.amazonaws.services.ec2.model.StartInstancesRequest;
-import com.amazonaws.services.ec2.model.StopInstancesRequest;
-import com.amazonaws.services.ec2.model.SummaryStatus;
-import com.amazonaws.services.ec2.model.Tag;
-import com.amazonaws.services.ec2.model.TagSpecification;
-import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 import com.amazonaws.util.EC2MetadataUtils;
-
-import net.schmizz.sshj.SSHClient;
-import net.schmizz.sshj.connection.channel.direct.Session;
-import net.schmizz.sshj.connection.channel.direct.Session.Command;
-import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
-import net.schmizz.sshj.xfer.FileSystemFile;
-import net.schmizz.sshj.xfer.scp.SCPFileTransfer;
 
 @Named
 public class MainInstance {
 	private static final String AWS_KEYPAIR_NAME = "accessibleFromMainInstance";
-	private static final String AMI_ID_EU_WEST_3_UBUNTU_SERVER_1804 = "ami-0a2ca21adb4a04084";
-	private static final String ERROR_INCORRECTLY_DEPLOYED = "The newly deployed EC2 instance did not start correctly. You may need to manually verify and correct this";
-	private static final String ERROR_STATUS_CHECKS_NOT_OK = "The newly deployed EC2 instance is running but some of the status checks may not have passed";
-	private static final int ITERATION_WAIT_TIME = 60 * 1000;
-	private static final int INSTANCE_PENDING = 0;
 	private static final int INSTANCE_RUNNING = 16;
-	private static final int INSTANCE_STOPPED = 80;
 	private static boolean keepAlive;
 	private static Instance mainInstance;
 	private static Instance shadow;
@@ -80,7 +46,6 @@ public class MainInstance {
 	private static final List<String> metricNames = Arrays.asList("CPUUtilization", "NetworkIn", "NetworkOut", "DiskReadOps", "DiskWriteOps");
 	private static Map<String, InstanceMetrics> metricsForInstances = new HashMap<>();
 	private static AWSCredentials credentials;
-	private static KeyPair javaKeyPair;
 	private static boolean mainInstanceStopAttempted = false;
 	private static boolean mainInstanceStartAttempted = false;
 	private static boolean mainInstanceRedeployAttempted = false;
@@ -122,7 +87,7 @@ public class MainInstance {
 					monitor();
 				}
 			}
-			waitUntilNextIteration();
+			EC2.waitUntilNextIteration();
 		}
 	}
 
@@ -139,69 +104,36 @@ public class MainInstance {
 		replaceMain = true;
 		if(mainInstanceStopAttempted == false) {
 			mainInstanceStopAttempted = true;
-			stopEC2Instance(mainInstance.getInstanceId());
+			EC2.stopEC2Instance(mainInstance.getInstanceId());
 		}
 		else {
 			if (mainInstanceStartAttempted == false) {
-				if (isStopped(mainInstance.getInstanceId())) {
+				if (EC2.isStopped(mainInstance.getInstanceId())) {
 					mainInstanceStartAttempted = true;
-					startEC2Instance(mainInstance.getInstanceId());
+					EC2.startEC2Instance(mainInstance.getInstanceId());
 				}
 			}
 			else {
 				if(mainInstanceRedeployAttempted == false) {
 					mainInstanceRedeployAttempted = true;
 					String previousMainInstanceId = mainInstance.getInstanceId();
-					mainInstance = deployDefaultEC2("", "cloudcomputing");
+					mainInstance = EC2.deployDefaultEC2("", "cloudcomputing");
 					// termination of non-working EC2 instance is not verified
 					// it might still be running in AWS which can be checked in AWS Console
-					terminateEC2(previousMainInstanceId);
+					EC2.terminateEC2(previousMainInstanceId);
 				}
 			}
 		}
 	}
 
-	private static void startEC2Instance(String instanceId) {
-		client.startInstances(new StartInstancesRequest().withInstanceIds(instanceId));
-	}
-
-	private static boolean isStopped(String instanceId) {
-		Instance stoppingInstance = retrieveEC2InstanceWithId(instanceId);
-		if (stoppingInstance.getState().getCode() == INSTANCE_STOPPED) {
-			return true;
-		}
-		return false;
-	}
-
-	private static void stopEC2Instance(String instanceId) {
-		client.stopInstances(new StopInstancesRequest().withInstanceIds(instanceId));
-	}
-
-	private static void terminateEC2(String instanceId) {
-		client.terminateInstances(new TerminateInstancesRequest().withInstanceIds(instanceId));
-	}
-
 	private static void updateEC2InstanceForMainInstance() {
-		mainInstance = retrieveEC2InstanceWithId(EC2MetadataUtils.getInstanceId());
-	}
-
-	private static Instance retrieveEC2InstanceWithId(String instanceId) {
-		return client.describeInstances(new DescribeInstancesRequest()
-				.withInstanceIds(instanceId))
-				.getReservations().get(0)
-				.getInstances().get(0);
+		mainInstance = EC2.retrieveEC2InstanceWithId(EC2MetadataUtils.getInstanceId());
 	}
 
 	private static boolean isMainInstanceAlive() {
 		updateEC2InstanceForMainInstance();
-		int httpStatus = healthCheckOnInstance(mainInstance);
+		int httpStatus = EC2.healthCheckOnInstance(mainInstance);
 		return mainInstance.getState().getCode() == INSTANCE_RUNNING && httpStatus == 204;
-	}
-
-	private static int healthCheckOnInstance(Instance instance) {
-		URI mainInstanceHealth = UriBuilder.fromUri(instance.getPublicDnsName()).path("health").build();
-		int httpStatus = ClientBuilder.newClient().target(mainInstanceHealth).request().get().getStatus();
-		return httpStatus;
 	}
 
 	public static boolean isAlive() {
@@ -232,180 +164,28 @@ public class MainInstance {
 		MainInstance.isShadow = isShadow;
 	}
 	
-	/**
-	 * Deploy an EC2 instance and wait for it to be running and the status checks to pass
-	 * 
-	 * @param usageTag is the tag that represents what the machine will be used for
-	 * @return the Instance object representing the deployed instance
-	 * @throws NoSuchAlgorithmException 
-	 * @throws IOException if the userdata script can not be read
-	 */
-	public static Instance deployDefaultEC2(String usageTag, String keyPairName) throws NoSuchAlgorithmException {
-		ensureJavaKeyPairExists();
-		RunInstancesRequest runInstancesRequest = new RunInstancesRequest(AMI_ID_EU_WEST_3_UBUNTU_SERVER_1804, 1, 1)
-				.withInstanceType(InstanceType.T2Micro)
-				.withKeyName(keyPairName)
-				.withUserData(getUserData());
-		if (usageTag != null && !usageTag.isEmpty()) {
-			runInstancesRequest.withTagSpecifications(
-					new TagSpecification()
-					.withResourceType(ResourceType.Instance)
-					.withTags(
-							new Tag()
-							.withKey("Usage")
-							.withValue(usageTag)));
-		}
-		RunInstancesResult runInstancesResult = client.runInstances(runInstancesRequest);
-		String deployedInstanceId = runInstancesResult.getReservation().getInstances().get(0).getInstanceId();
-		// wait up to 1 minute for the instance to run
-		waitForInstanceToRun(deployedInstanceId);
-		return client.describeInstances(new DescribeInstancesRequest().withInstanceIds(deployedInstanceId))
-				.getReservations().get(0)
-				.getInstances().get(0);
-	}
-
-	private static void ensureJavaKeyPairExists() throws NoSuchAlgorithmException {
-		if (javaKeyPair != null) {
-			return;
-		}
-		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-		generator.initialize(2048);
-		javaKeyPair = generator.generateKeyPair();
-		String publicKey = Base64.getEncoder().encodeToString(javaKeyPair.getPublic().getEncoded());
-		removeExistingKeyPair();
-		ImportKeyPairRequest keyPairRequest = new ImportKeyPairRequest(AWS_KEYPAIR_NAME, publicKey);
-		client.importKeyPair(keyPairRequest);
-	}
-
-	private static void removeExistingKeyPair() {
-		client.deleteKeyPair(new DeleteKeyPairRequest(AWS_KEYPAIR_NAME));
-	}
-
-	private static String getUserData() {
-		String userData = "#!/bin/bash\n" + 
-				"apt update\n" + 
-				"apt install -y openjdk-8-jre\n";
-		return Base64.getEncoder().encodeToString(userData.getBytes());
-	}
-
-	private static void waitForInstanceToRun(String deployedInstanceId) {
-		for (int i = 0; i < 6; i++) {
-			try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			int state = getInstanceState(deployedInstanceId);
-			if (state == INSTANCE_PENDING) {
-				continue;
-			}
-			if (state == INSTANCE_RUNNING) {
-				// check status checks are completed as well
-				boolean passed = verifyStatusChecksPassed(deployedInstanceId);
-				if (!passed) {
-				        throw new IllegalStateException(ERROR_STATUS_CHECKS_NOT_OK);
-				}
-				break;
-			}
-			throw new IllegalStateException(ERROR_INCORRECTLY_DEPLOYED);
-		}
-	}
-
-	private static int getInstanceState(String deployedInstanceId) {
-		int state = client.describeInstances(new DescribeInstancesRequest().withInstanceIds(deployedInstanceId))
-				.getReservations().get(0)
-				.getInstances().get(0)
-				.getState()
-				.getCode();
-		return state;
-	}
-	
-	private static boolean verifyStatusChecksPassed(String deployedInstanceId) {
-		boolean passed = false;
-		// wait up to 10 minutes for the status checks
-		for (int j = 0; j < 10; j++) {
-			try {
-				Thread.sleep(60000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			InstanceStatus status = client
-					.describeInstanceStatus(new DescribeInstanceStatusRequest().withInstanceIds(deployedInstanceId))
-					.getInstanceStatuses().get(0);
-			SummaryStatus systemStatus = SummaryStatus.fromValue(status.getSystemStatus().getStatus());
-			SummaryStatus instanceStatus = SummaryStatus.fromValue(status.getInstanceStatus().getStatus());
-			if (SummaryStatus.Ok.equals(systemStatus) && SummaryStatus.Ok.equals(instanceStatus)) {
-				passed = true;
-				break;
-			}
-		}
-		return passed;
-	}
-
 	private static void deployShadow() throws IOException, NoSuchAlgorithmException {
-		shadow = deployDefaultEC2("shadow", AWS_KEYPAIR_NAME);
+		shadow = EC2.deployDefaultEC2("shadow", AWS_KEYPAIR_NAME);
 		System.out.println("Shadow deployed");
-		waitForInstanceToRun(shadow.getInstanceId());
-		copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/application.jar").toFile(), shadow);
-		copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/app-orchestrator.jar").toFile(), shadow);
-		copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/load-balancer.jar").toFile(), shadow);
-		copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/main-instance.jar").toFile(), shadow);
-		startDeployedApplication(shadow, "main-instance");
+		EC2.waitForInstanceToRun(shadow.getInstanceId());
+		EC2.copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/application.jar").toFile(), shadow);
+		EC2.copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/app-orchestrator.jar").toFile(), shadow);
+		EC2.copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/load-balancer.jar").toFile(), shadow);
+		EC2.copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/main-instance.jar").toFile(), shadow);
+		EC2.startDeployedApplication(shadow, "main-instance");
 		System.out.println("Shadow application started");
 	}
 
-	private static void copyApplicationToDeployedInstance(File applicationJarFile, Instance instance) throws IOException, NoSuchAlgorithmException {
-		ensureJavaKeyPairExists();
-		try (SSHClient ssh = new SSHClient()){
-			ssh.loadKnownHosts();
-			ssh.addHostKeyVerifier(new PromiscuousVerifier());
-			ssh.connect(instance.getPublicDnsName());
-			ssh.authPublickey("ubuntu", Arrays.asList(ssh.loadKeys(javaKeyPair)));
-			SCPFileTransfer scp = ssh.newSCPFileTransfer();
-			String homeDirOnRemoteInstance = "/home/ubuntu/";
-			scp.upload(new FileSystemFile(applicationJarFile), homeDirOnRemoteInstance);
-		}
-	}
-
-	private static void startDeployedApplication(Instance instance, String applicationName) throws IOException, NoSuchAlgorithmException {
-		ensureJavaKeyPairExists();
-		try (SSHClient ssh = new SSHClient()){
-			ssh.loadKnownHosts();
-			ssh.addHostKeyVerifier(new PromiscuousVerifier());
-			ssh.connect(instance.getPublicDnsName());
-			ssh.authPublickey("ubuntu", Arrays.asList(ssh.loadKeys(javaKeyPair)));
-			Command command;
-			String remoteCommand = "nohup java -jar /home/ubuntu/"+applicationName+".jar > /home/ubuntu/"+applicationName+".log 2>&1 &";
-			try(Session session = ssh.startSession()){
-				command = session.exec(remoteCommand);
-			}
-			if (command.getExitStatus() != 0) {
-				System.out.println(applicationName + " was not started correctly with the command: " + remoteCommand);
-			}
-		}
-	}
-	
 	private static void deployAppOrchestrator() throws IOException, NoSuchAlgorithmException {
-		ensureJavaKeyPairExists();
-		appOrchestrator = deployDefaultEC2("App Orchestrator", AWS_KEYPAIR_NAME);
+		EC2.ensureJavaKeyPairExists();
+		appOrchestrator = EC2.deployDefaultEC2("App Orchestrator", AWS_KEYPAIR_NAME);
 		System.out.println("App Orchestrator deployed");
-		waitForInstanceToRun(appOrchestrator.getInstanceId());
-		copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/application.jar").toFile(), appOrchestrator);
-		copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/load-balancer.jar").toFile(), appOrchestrator);
-		copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/app-orchestrator.jar").toFile(), appOrchestrator);
-		startDeployedApplication(appOrchestrator, "app-orchestrator");
+		EC2.waitForInstanceToRun(appOrchestrator.getInstanceId());
+		EC2.copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/application.jar").toFile(), appOrchestrator);
+		EC2.copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/load-balancer.jar").toFile(), appOrchestrator);
+		EC2.copyApplicationToDeployedInstance(Paths.get("/home/ubuntu/app-orchestrator.jar").toFile(), appOrchestrator);
+		EC2.startDeployedApplication(appOrchestrator, "app-orchestrator");
 		System.out.println("App Orchestrator started");
-	}
-
-	/**
-	 * Wait a specific amount of time before
-	 */
-	private static void waitUntilNextIteration() {
-		try {
-			Thread.sleep(ITERATION_WAIT_TIME);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 	}
 
 	public static AWSCredentials getCredentials() {
